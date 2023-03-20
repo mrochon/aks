842e7071bea3056627c9cb87bb7c3446ece6e1fd
MariusR <sailingrock@live.com>
2023-03-20 15:27:37 -0700

    Minor debug tweaks

--- APIM/debug.ps1
+++ APIM/debug.ps1
@@ -1,5 +1,11 @@
-kubectl port-forward pod/weather-api-99c9c7fd6-2v5cf  8080:80 -n app
+kubectl get svc -n ingress-nginx
+
+kubectl get pods -n app
+kubectl port-forward pod/weather-api-99c9c7fd6-l4hfb  8080:80 -n app
 curl -kL http
+
+kubectl get svc -n app
+kubectl port-forward 10.0.40.161  8080:80 -n app
 
 # localhost:8080/weatherforecast
 
@@ -8,8 +14,9 @@ kubectl get service ingress-nginx-controller --namespace=ingress-nginx
 # 104.42.145.179:32294/TCP,443:31047/TCP
 # 104.42.145.179   80:32294/TCP,443:31047/TCP
 
-
-kubectl get pods --namespace ingress-nginx
 kubectl logs ingress-nginx-controller-89758f7c6-4b8kc --namespace ingress-nginx
 
 kubectl exec -ti ingress-nginx-controller-89758f7c6-4b8kc -n ingress-nginx -- bash
+
+kubectl get pods -n ingress-nginx
+kubectl logs nginx-ingress-ingress-nginx-controller-7b5fd4596d-sb4lh -n ingress-nginx
---
+++ APIM/yaml/Readme.md
@@ -0,0 +1,2 @@
+Error: INSTALLATION FAILED: Kubernetes cluster unreachable: Get "https://aks-aksdev-rg-aksdevsecops-568158-shr74lkt.hcp.westus.azmk8s.io:443/version": dial tcp: lookup aks-aksdev-rg-aksdevsecops-568158-shr74lkt.hcp.westus.azmk8s.io: no such host
+PS C:\Users\mrochon\source\repos\aks>
